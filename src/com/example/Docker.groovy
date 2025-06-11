f3c599ede461c673656b74a82ba2b6996bf99901
Nella1a <m.allen@gmx.at>
2025-06-11 16:55:51 +0200

    Fix missing script.method

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -5,14 +5,14 @@ class Docker implements Serializable {
 
     def script
 
-    Docker(script) {
+    Docker (script) {
         this.script = script
     }
 
 
     def buildDockerImage(String imageName) {
         script.echo "building the docker image"
-        script.([script.usernamePassword(credentialsId: "docker-hub-repo", passwordVariable: 'PASS', usernameVariable: 'USER')]){
+        script.withCredentials([script.usernamePassword(credentialsId: "docker-hub-repo", passwordVariable: 'PASS', usernameVariable: 'USER')]){
 
             script.sh "docker build -t $imageName ."
             script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
